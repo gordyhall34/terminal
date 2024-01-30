2e2d174fd61ba408be678615db6551f89fb10e7f
Rob Howell <rbh3457@gmail.com>
2024-01-29 20:42:02 -0500

    Add files via upload

---
+++ term.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    while (true)
+    {
+        string line;
+        cout << "<< ";
+        cin >> line;
+    }
+}
+
+int help()
+{
+    
+}
